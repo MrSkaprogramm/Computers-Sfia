dfe8aed61b0081286dc93fd9cbd2806cba6e18fc
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-03-26 00:02:22 +0300

    Create XMLParser.java

---
+++ src/main/java/by/epam/tr/XMLParser.java
@@ -0,0 +1,11 @@
+package by.epam.tr;
+
+import by.epam.tr.view.ApplicationView;
+
+public class XMLParser {
+
+  public static void main(String[] args) {
+    ApplicationView applicationView = new ApplicationView();
+    applicationView.applicationMenu();
+  }
+}
